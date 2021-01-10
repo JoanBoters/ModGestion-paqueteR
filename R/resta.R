61deccabd8de5bbb520fb2b880f8dfd7251619a4
Joan Boters <al403192@uji.es>
2021-01-10 21:23:46 +0100

    Commit Inicial

---
+++ R/resta.R
@@ -0,0 +1,17 @@
+#' Resta entre dos números
+#'
+#' Esta función devuelve la resta de dos números
+#'
+#' @param a Primer número
+#' @param b Segundo número
+#'
+#' @return La resta entre a y b
+#' @export
+#'
+#' @examples
+#' resta(5,3)
+#'
+#' @seealso \link{suma}
+resta=function(a,b){
+  return(a-b)
+}
